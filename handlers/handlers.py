70b52fdf52e26f0ae1b65cbbda3f7b8942882dec
Kirill Pogozheff <kpogozhev@nes.ru>
2023-05-04 16:39:18 +0300

    Fix handlers

--- handlers/handlers.py
+++ handlers/handlers.py
@@ -1,26 +1,26 @@
 import json
 
-from db.mongo import mongo_users, do_find_one, do_insert_one, do_find_many, do_delete_one
+from db.mongo import users, do_find_one, do_insert_one, do_find_many, do_delete_one
 
 
 async def enter_username(data: dict):
-    current_char = await do_find_one({'_id': data['message']['chat']['id']}, mongo_users)
+    current_char = await do_find_one({'_id': data['message']['chat']['id']}, users)
     if current_char is None:
         await do_insert_one({'_id': data['message']['chat']['id'],
-                             'name': data['message']['text']}, mongo_users)
+                             'name': data['message']['text']}, users)
         return 'Персонаж **{}** успешно создан!'.format(data['message']['text'])
     return None
 
 
 async def get_users_list():
-    users_list = await do_find_many({}, mongo_users)
+    users_list = await do_find_many({}, users)
     return str(users_list)
 
 
 async def delete_user(data: dict):
-    current_char = await do_find_one({'_id': data['message']['chat']['id']}, mongo_users)
+    current_char = await do_find_one({'_id': data['message']['chat']['id']}, users)
     if current_char is not None:
-        await do_delete_one({'_id': data['message']['chat']['id']}, mongo_users)
+        await do_delete_one({'_id': data['message']['chat']['id']}, users)
         return 'Персонаж **{}** успешно удален!'.format(current_char['name'])
     else:
         return 'Вы еще не создали персонажа. Чтобы начать используйте /start'
